46f7abbe4e63a4f7ae1a781a821ebeee8e90ecae
Derrick Yoo <derrickyoo@gmail.com>
2026-02-28 10:36:40 -0800

    import from eslint plugin security

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from "@eslint/js";
+import pluginSecurity from "eslint-plugin-security";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
